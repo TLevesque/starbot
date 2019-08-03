874c9dcdd14b861b39a2d7f9824bf69c93ae2855
Thibaut Levesque <levesque.thibaut@gmail.com>
2019-08-03 23:32:53 +0200

    feat: second commit

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ if (config('PROXY_URI')) {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => { res.send('\n 👋 🌍 👋 🌍 👋 \n') })
+app.get('/', (req, res) => { res.send('\n 👋 🌍 👋 🌍 👋 👋 🌍 👋 🌍 👋 \n') })
 
 app.post('/commands/starbot', (req, res) => {
   let payload = req.body
